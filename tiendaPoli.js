11fc87894835e6b197354003b8498d931db3c19e
ignacio tosini <ignaciotosini2002@gmail.com>
2023-01-31 12:19:54 -0300

    cambios en tienda

--- tiendaPoli.js
+++ tiendaPoli.js
@@ -1,12 +1,13 @@
 class Producto {
-    constructor(nombreProducto, precio, stock) {
+    constructor(nombreProducto, precio, stock, imagenProducto) {
         this.nombreProducto = nombreProducto;
         this.precio = precio;
         this.stock = stock;
+        this.imagenProducto = imagenProducto;
     }
 }
 
-class Cliente { /**No estoy seguro si lo voy a usar */
+class Cliente {
     constructor(nombre, apellido, contrasenia) {
         this.nombre = nombre;
         this.apellido = apellido;
@@ -14,19 +15,59 @@ class Cliente { /**No estoy seguro si lo voy a usar */
     }
 }
 
+function renderProducto(){
+    containerMainTienda.innerHTML = "";
+
+    for (const producto of tienda){
+        const div = document.createElement("div");
+        div.className = "containerProducto";
+        const pNombre = document.createElement("p");
+        pNombre.className = "nombreProducto";
+        pNombre.innerHTML = `${producto.nombreProducto}`;
+        const img = document.createElement("img");
+        img.className = "img";
+        img.src = producto.imagenProducto;
+        const botonMas = document.createElement("button");
+        botonMas.className = "button";
+        botonMas.innerHTML = "+";
+        const contador = document.createElement("p");
+        contador.className = "contador";
+        contador.innerHTML = 0;
+        const botonMenos = document.createElement("button");
+        botonMenos.className = "button";
+        botonMenos.innerHTML = "-";
+
+        div.append(pNombre);
+        div.append(img);
+        div.append(botonMas);
+        div.append(contador);
+        div.append(botonMenos);
+        containerMainTienda.append(div);
+    }
+}
+
+function agregarProducto(nombre, precio, stock, imagenProducto) {
+    const producto = new Producto(nombre, precio, stock, imagenProducto);
+    producto.nombreProducto = producto.nombreProducto.charAt().toUpperCase() + producto.nombreProducto.slice(1);
+    tienda.push(producto);
+    renderProducto();
+}
+
 let total = 0;
 let continuar = "s";
 
-let tienda = [];
-tienda.push(new Producto("remera", 200, 10));
-tienda.push(new Producto("buzo", 500, 50));
-tienda.push(new Producto("pantalon", 300, 5));
-tienda.push(new Producto("campera", 800, 8));
-tienda.push(new Producto("mochila", 1200, 3));
-tienda.push(new Producto("calculadora", 2000, 15));
-tienda.push(new Producto("lapices", 100, 100));
-tienda.push(new Producto("goma", 200, 60));
-tienda.push(new Producto("cuaderno", 400, 80));
+const containerMainTienda = document.getElementById("containerMainTienda");
+const tienda = [];
+
+agregarProducto("remera", 200, 10, "../img/imgRemeraMangaLargaDanza.jpg");
+agregarProducto("buzo", 500, 50, "../img/imgBuzoPolivalenteDeArte.jpg");
+agregarProducto("pantalon", 300, 5, "../img/remerasDelPoli.jpg");
+agregarProducto("campera", 800, 8, "../img/visualesUno.jpeg");
+agregarProducto("mochila", 1200, 3, "../img/imgBuzoPolivalenteDeArte.jpg");
+agregarProducto("calculadora", 2000, 15, "../img/imgBuzoPolivalenteDeArte.jpg");
+agregarProducto("lapices", 100, 100, "../img/imgBuzoPolivalenteDeArte.jpg");
+agregarProducto("goma", 200, 60, "../img/imgBuzoPolivalenteDeArte.jpg");
+agregarProducto("cuaderno", 400, 80, "../img/imgBuzoPolivalenteDeArte.jpg");
 
 const calcularTotal = (cantidad, precio) => {
     let iva = 0.21;
@@ -49,7 +90,7 @@ const calcularCantidad = (nombre, precio, stock) => {
     return cantidad;
 }
 
-do {
+/* do {
     console.log(`Los produtos son:`);
     for (const productos of tienda) {
         console.log(`${productos.nombreProducto}, el stock es de: ${productos.stock}.`);
@@ -68,3 +109,4 @@ do {
     continuar = prompt("Escriba 's' si desea continuar comprando");
 } while (continuar == "s" || continuar == "S");
 alert("Adios, espero que le haya gustado el servicio, vuelva pronto");
+ */
